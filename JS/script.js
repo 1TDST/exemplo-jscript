70a40ef53cfca4130d02b9732a37d47d8bab92e4
CarvalhoGabriell <gabrielfernandes2312@gmial.com>
2020-05-17 18:15:18 -0300

    Etilizando paginas com img e gifs

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,26 @@
 
-trocarImg();
+
 
 function trocarImg() {
-    document.getElementById('lateral1').src = ""
+    document.getElementById('lateral1').src = "./img/will.gif"
+    document.getElementById('lateral2').src = "./img/banner.gif"
 }
+trocarImg();
+
+function rodapeImg() {
+  document.getElementById('rodape').src = "./img/toque.gif"
+}
+rodapeImg();
+
+function cabecalho() {
+  document.getElementById('header1').src = "./img/space.jpg"
+
+}
+cabecalho();
+
+
+function principal() {
+  document.getElementById('corpo').src = "./img/tio_phill.gif"
+}
+
+principal();
